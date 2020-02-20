05d2b4212fd85892fa9afe26ba2bfbe61a525d85
Alina <alinazarik92@gmail.com>
2020-02-20 23:45:49 +0300

    Личный проект: больше деталей (часть 1)

---
+++ js/main.js
@@ -0,0 +1,66 @@
+'use strict';
+
+var MESSAGES = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
+var NAMES = ['Иван', 'Ира', 'Галя', 'Толя', 'Милана', 'Оливия', 'Крис', 'Кемаль', 'Якоб', 'Джоанна', 'Вера', 'Айсылу'];
+
+var getRandomNumber = function (min, max) {
+  var randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+
+  return randomNumber;
+};
+
+var getRandomElement = function (min, arr) {
+  var randomI = Math.floor(Math.random() * (arr.length - min)) + min;
+  var randomElement = arr[randomI];
+
+  return randomElement;
+};
+
+var getCommentsList = function () {
+  var randomComments = [];
+  for (var i = 0; i < getRandomNumber(1, 4); i++) {
+    randomComments[i] = {
+      avatar: 'img/avatar-' + getRandomNumber(1, 6) + '.svg',
+      message: getRandomElement(0, MESSAGES),
+      name: getRandomElement(0, NAMES)
+    };
+  }
+  return randomComments;
+};
+
+var photos = [];
+
+for (var i = 0; i < 25; i++) {
+  photos[i] = {
+    url: 'photos/' + (i + 1) + '.jpg',
+    description: '',
+    likes: getRandomNumber(15, 200),
+    comments: getCommentsList()
+  };
+}
+
+var pictureContainerElement = document.querySelector('.pictures');
+
+var pictureTemplate = document.querySelector('#picture')
+    .content
+    .querySelector('.picture');
+
+var fragment = document.createDocumentFragment();
+
+photos.forEach(function (photo) {
+  var pictureElement = pictureTemplate.cloneNode(true);
+
+  pictureElement.querySelector('.picture__img').src = photo.url;
+  pictureElement.querySelector('.picture__likes').textContent = photo.likes;
+  pictureElement.querySelector('.picture__comments').textContent = photo.comments.length;
+
+  fragment.appendChild(pictureElement);
+});
+pictureContainerElement.appendChild(fragment);
